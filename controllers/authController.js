75d91a2046a5de7c7a732f178194f6dca7d72344
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-27 12:13:29 +0530

    create registerAuth

---
+++ controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    if (!username || !email || !password) {
+      res.status(400).json({ massage: "All fields are required" });
+    }
+    const user = await User.findOne({ username });
+
+    if (user) {
+      res.status(400).json({ massage: "User already exist" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    return res.status(200).json({ massage: "Account Create Succesfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error : "Internal Server Error"});
+  }
+};
+
+module.exports = {register}
