4ea7702154e9c7ec1e959683701fe6a7f98ab7f5
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-29 00:40:56 +0530

    create router

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -2,8 +2,11 @@ const exprress = require("express");
 const router = exprress.Router();
 
 const {createJob} = require("../controllers/jobController");
+const {getJob} = require("../controllers/jobController");
+
 
 //use controller as a callback
 router.post('/', createJob);
+router.get('/', getJob);
 
 module.exports = router;
