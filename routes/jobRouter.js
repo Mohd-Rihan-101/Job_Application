5a24cb5e044e218f942bcfa5f11656dfb114a91c
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-29 12:14:00 +0530

    add router

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -4,9 +4,12 @@ const router = exprress.Router();
 const { createJob } = require("../controllers/jobController");
 const { getJob } = require("../controllers/jobController");
 const { updateJob } = require("../controllers/jobController");
+const { deletJob } = require("../controllers/jobController");
+
 
 //use controller as a callback
 router.post("/", createJob);
 router.get("/", getJob);
 router.put("/:id", updateJob);
+router.delete('/:id', deletJob);
 module.exports = router;
